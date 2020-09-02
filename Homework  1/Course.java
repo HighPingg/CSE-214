8b3ee6b7ad39f11157b0b02f4dbd5ae0c3d79192
Vincent Zheng <zhengvincent0825@gmail.com>
2020-09-02 00:03:45 -0400

    file headings and empty class files

---
+++ Homework  1/Course.java
@@ -0,0 +1,13 @@
+/**
+* @Name Vincent Zheng
+* @SOLAR_ID 113469839
+* @Email vincent.zheng@stonybrook.edu
+* @Assignment 
+* @Course CSE 214
+* @Recitation R02
+* @TA William Simunek
+*/
+
+class Course {
+    
+}
---
+++ Homework  1/Planner.java
@@ -0,0 +1,14 @@
+/**
+* @Name Vincent Zheng
+* @SOLAR_ID 113469839
+* @Email vincent.zheng@stonybrook.edu
+* @Assignment 
+* @Course CSE 214
+* @Recitation R02
+* @TA William Simunek
+*/
+
+
+class Planner {
+    
+}
